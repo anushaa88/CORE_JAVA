ac6295d42c95d5c09b43d6081fb8c574b60ccad3
SaiAnusha_Mirthipati <saianushamirthipati@gmail.com>
2024-01-16 23:01:43 -0600

    Added Loops files

---
+++ Loop/src/Age.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Age {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your age: ");
+        int age = sc.nextInt();
+        if (age < 13) {
+            System.out.println("Kid");
+        } else if (age >= 13 && age <= 19) {
+            System.out.println("Teen");
+        } else {
+            System.out.println("Adult");
+        }
+        sc.close();
+    }
+}

9f851e4e282ea56ea7ad7930e5c992565c4b9ba3
SaiAnusha_Mirthipati <saianushamirthipati@gmail.com>
2024-01-31 12:11:29 -0600

    Added additional files

---
+++ Maven1/src/test/java/test_additional/FactorialTest.java
@@ -0,0 +1,8 @@
+package additional;
+public class FactorialTest {
+    public static void main(String[] args) {
+        int number = 5;
+        int result = Factorial.factorial(number);
+        System.out.println("Factorial of " + number + " is: " + result);
+    }
+}
---
+++ Maven1/src/test/java/test_additional/RepeatedCharactersTest.java
@@ -0,0 +1,8 @@
+package additional;
+public class RepeatedCharactersTest {
+
+    public static void main(String[] args) {
+        String inputString = "hello world";
+        RepeatedCharacters.printRepeatedCharacters(inputString);
+    }
+}
